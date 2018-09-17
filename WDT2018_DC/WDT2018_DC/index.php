5505c570dff6dcd3eed3c40938df318aa1d63b06
ZSlongtalk <californiazds@gmail.com>
2018-09-17 09:58:30 -0600

    added php and pages folder

---
+++ WDT2018_DC/WDT2018_DC/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Washington DC Nationals Guide</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/css?family=PT+Sans|Slabo+27px" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" type="text/css" href="css/w3.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+  <h1>Hello world</h1>
+  <?php echo "php works";?>
+</body>
+</html>
---
+++ WDT2018_DC/WDT2018_DC/pages/transport.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Transport: Washington DC Nationals</title>
+</head>
+<body>
+
+</body>
+</html>
