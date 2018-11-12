08add4b12c1391ef4b614a5495475efbebbdbe5b
ZSlongtalk <californiazds@gmail.com>
2018-11-12 10:48:33 -0700

    add w3 header and css3 grids
    added bare-bones webpages to the pages folder

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Washington DC Nationals Guide</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/css?family=PT+Sans|Slabo+27px" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/w3.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+  <div class="w3-bar w3-indigo">
+    <h1 id="header">Welcome to the 2020 NLC at Washington D.C. Informatation Page</h1>
+    <hr>
+    <a href="index.html" class="w3-bar-item w3-button">Home</a>
+    <a href="pages/transportation.php" class="w3-bar-item w3-button">Transportation</a>
+    <a href="pages/hotel.php" class="w3-bar-item w3-button">Hotel</a>
+    <a href="pages/activities.php" class="w3-bar-item w3-button">Activities</a>
+  </div>
+  <div class="w3-container">
+    <div class="grid-container">
+      <div id="sidebar" class="gridStyle">
+        <?php
+          echo "php works";
+          echo "<br>background color is just a test";
+        ?>
+      </div>
+      <div id="main" class="gridStyle">
+        <h2>Main informatation</h2>
+      </div>
+      <div id="base" class="gridStyle">
+        <p>This is the other informatation box</p>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
---
+++ pages/activities.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Washington DC Nationals Guide: Transportation</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/css?family=PT+Sans|Slabo+27px" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="../css/w3.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" type="text/css" href="../css/main.css">
+</head>
+<body>
+  <div class="w3-bar w3-indigo">
+    <a href="../index.php" class="w3-bar-item w3-button">Home</a>
+    <a href="#" class="w3-bar-item w3-button">Link 1</a>
+    <a href="#" class="w3-bar-item w3-button">Link 2</a>
+    <a href="#" class="w3-bar-item w3-button">Link 3</a>
+  </div>
+  <div class="w3-container">
+  <h1>Hello world</h1>
+  <?php
+    echo "php works";
+  ?>
+</div>
+</body>
+</html>
