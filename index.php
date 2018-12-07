a1c3b26e96c9214c61920af3f44a245f8363434c
ZSlongtalk <californiazds@gmail.com>
2018-12-06 17:29:17 -0700

    Bootstrap intergrated

--- index.php
+++ index.php
@@ -11,12 +11,22 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link rel="stylesheet" type="text/css" href="css/w3.css">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="css/main.css">
+  <link href="css/cover.css" rel="stylesheet">
+  <style>
+    #demo {
+      background-color: #C7D1D7;
+    }
+  </style>
 </head>
 <body>
   <div class="w3-bar w3-red">
     <h1 id="header">Welcome to the 2020 NLC at Washington D.C. Informatation Website</h1>
-    <div class="w3-bar w3-indigo">
+    <div class="w3-bar LinkColor">
       <a href="index.html" class="w3-bar-item w3-button w3-hover-blue">Home</a>
       <a href="pages/event.php" class="w3-bar-item w3-button w3-hover-blue">NLC</a>
       <a href="pages/transportation.php" class="w3-bar-item w3-button w3-hover-blue">Transportation</a>
@@ -43,11 +53,50 @@
         ?>
       </div>
       <div id="main" class="gridStyle">
-        <h2>Main informatation</h2>
+        <div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/placeholder1.jpg" alt="Los Angeles">
+      <div class="carousel-caption">
+        <h3>Los Angeles</h3>
+        <p>We had such a great time in LA!</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/placeholder2.jpg" alt="Chicago">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="images/placeholder3.jpg" alt="New York">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
         <?php
           session_start();
           if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
             echo "<h2>Hello ". $_SESSION["username"] . "</h2>";
+          }
+          if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+            echo "<h2>Feel free to log in or create an account to gain access to all of our features<br>";
+            echo "<a href='php/login.php' class='w3-button userFeatures'>Log In</a> <a href='php/createUser.php' class='w3-button userFeatures'>Create User</a></h2>";
           }
         ?>
         <!-- This only will occur when users login to the site -->
--- pages/QandA.php
+++ pages/QandA.php
@@ -11,6 +11,9 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link rel="stylesheet" type="text/css" href="../css/w3.css">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="../css/main.css">
   <style>
     h2 {
